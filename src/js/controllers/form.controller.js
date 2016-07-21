4a3e31612e43504235dca57924b4f886ef315df2
Christopher Herrington <christopher.herrington@outlook.com>
2016-07-21 09:32:36 -0400

    Fixed some display errors including misaligned error messages

--- src/js/controllers/form.controller.js
+++ src/js/controllers/form.controller.js
@@ -12,8 +12,8 @@ $scope.addForm = function (form) {
   console.log("Sending POST request")
   $http.post(SERVER.URL, form).then(function (res) {
     console.log(" Server status code ", res.status, res.statusText,"\n\n" , "Data Received: " , res.data )
-        // $scope.forms.push(res.data);
-        // $scope.form = {};
+        $scope.forms.push(res.data);
+        $scope.form = {};
       });
 }
 
